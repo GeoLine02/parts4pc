65d0a13cfe5a64e06c2bc4b2f45de9b68855b619
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2025-09-26 14:32:03 +0400

    add retun in route hanlder

--- app/api/temporary/vip-categories/route.ts
+++ app/api/temporary/vip-categories/route.ts
@@ -1,8 +1,15 @@
 import Products from "@/models/products";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
     await Products.bulkCreate([]);
+    return NextResponse.json(
+      {
+        message: "seed added",
+      },
+      { status: 201 }
+    );
   } catch (error) {
     console.log(error);
   }
